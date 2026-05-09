88f1ab0ac3bc711d2e9c92adbd359b675f27b603
Dolla Grace <dollagraceambwaya@gmail.com>
2026-05-10 00:50:49 +0300

    fix: set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/iyf-s10-week-08-dollagraceambwaya-commits/",
+});
