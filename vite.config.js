7510cef730fd12059989e86058b4e910a57ac7cb
Dolla Grace <dollagraceambwaya@gmail.com>
2026-05-10 01:02:21 +0300

    fix: correct asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iyf-s10-week-08-dollagraceambwaya-commits/",
+  base: "/iyf-s10-week-08-dollagraceambwaya-commits/", // 👈 must match your repo name
 });
